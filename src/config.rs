17b477b7c58e5cb1c201277c8782349385017222
Kornel <kornel@geekhood.net>
2025-04-05 20:14:01 +0100

    Use the existing trait

--- src/config.rs
+++ src/config.rs
@@ -17,16 +17,14 @@ use rayon::prelude::*;
 use std::borrow::Cow;
 use std::collections::{BTreeSet, HashSet};
 use std::env::consts::{DLL_PREFIX, DLL_SUFFIX, EXE_SUFFIX};
-use std::ffi::OsStr;
 use std::io::Write;
-use std::os::unix::ffi::OsStrExt;
 use std::path::{Component, Path, PathBuf};
 use std::process::Command;
 use std::time::SystemTime;
 use std::{fs, io};
 
-pub(crate) fn is_glob_pattern(s: impl AsRef<OsStr> + Sized) -> bool {
-    s.as_ref().as_bytes().iter().any(|&c| c == b'*' || c == b'[' || c == b']' || c == b'!')
+pub(crate) fn is_glob_pattern(s: impl AsRef<Path> + Sized) -> bool {
+    s.as_ref().to_bytes().iter().any(|&c| c == b'*' || c == b'[' || c == b']' || c == b'!')
 }
 
 /// Match the official `dh_installsystemd` defaults and rename the confusing
